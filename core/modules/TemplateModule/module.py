ca70f2d7dcbc8085463dd8e306997a573fd70710
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2020-02-20 16:52:35 +0300

    Нет, проблем и фиксов три. Четыре+два!

--- core/modules/TemplateModule/module.py
+++ core/modules/TemplateModule/module.py
@@ -1,10 +1,7 @@
 import discord
 
 from typing import Dict
-from core.bot import Bot
-from core.command import Command
-from core.command_result import CommandResult
-from core.modulebase import ModuleBase
+from core import ModuleBase, CommandResult, Command, Bot
 
 
 class TemplateCommand(Command):
