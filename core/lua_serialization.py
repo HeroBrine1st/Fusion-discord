084e673c967a3ec65a7dfadd1396be1f8823efba
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2020-02-22 15:13:58 +0300

    Допилили библиотеку сериализации. убрали авторский говнокод

--- core/lua_serialization.py
+++ core/lua_serialization.py
@@ -7,23 +7,9 @@ def type_of(v, *types):
     return any(isinstance(v, t) for t in types)
 
 
-def get_delim(s):
-    if '"' not in s and "\n" not in s:
-        return '"', '"'
-    for op, cl in SPECIAL_DELIM:
-        if op not in s and cl not in s:
-            return op, cl
-    raise ValueError("could not find delimiter for string")
-
-
-def indent(s, level, prefix="  "):
-    return "\n".join("{}{}".format(prefix * level, elem).rstrip() for elem in s.split("\n"))
-
-
 def to_lua(value):
     if type_of(value, str):
-        od, cd = get_delim(value)
-        temp = "{}{}{}".format(od, value, cd)
+        temp = "\"%s\"" % value.replace("\"", "\\\"")
     elif type_of(value, float, int):
         temp = "{}".format(value)
     elif type_of(value, dict):
@@ -32,20 +18,20 @@ def to_lua(value):
             v = value[k]
             ks = "{}".format(to_lua(k))
             if ks.startswith("["):
-                ks = "[ {} ]".format(ks)
+                ks = "[{}]".format(ks)
             else:
                 ks = "[{}]".format(ks)
             vs = to_lua(v)
-            kvs.append("{} = {}".format(ks, vs))
-        temp = "{{\n{}\n}}".format(indent(",\n".join(kvs), 1))
+            kvs.append("{}={}".format(ks, vs))
+        temp = "{{{}}}".format(",".join(kvs))
     elif type_of(value, list, tuple, set):
         kvs = []
         for i, v in enumerate(value):
             ks = "[{}]".format(i + 1)
             vs = to_lua(v)
-            kvs.append("{} = {}".format(ks, vs))
-        temp = "{{\n{}\n}}".format(indent(",\n".join(kvs), 1))
+            kvs.append("{}={}".format(ks, vs))
+        temp = "{{{}}}".format(",".join(kvs))
     else:
-        temp = to_lua(str(value))
+        raise ValueError
 
     return temp
